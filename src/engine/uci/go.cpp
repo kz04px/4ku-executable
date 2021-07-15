5d63d4df15100bcddd0d67b28a22ef93309c8a60
kz04px <6536046+kz04px@users.noreply.github.com>
2021-07-15 19:40:53 +0100

    Merge change from master branch

--- src/engine/uci/go.cpp
+++ src/engine/uci/go.cpp
@@ -7,7 +7,7 @@
 namespace uci {
 
 void go(chess::Position &pos, const int time) {
-    char movestr[6];
+    char bestmove_str[] = "bestmove 123456";
 
     const auto stop_time = clock() + time / 30'000.0f * CLOCKS_PER_SEC;
 
@@ -20,10 +20,10 @@ void go(chess::Position &pos, const int time) {
             break;
         }
 
-        chess::move_str(bestmove, movestr, pos.flipped);
+        chess::move_str(bestmove, &bestmove_str[9], pos.flipped);
     }
 
-    printf("bestmove %s\n", movestr);
+    puts(bestmove_str);
 }
 
 }  // namespace uci
