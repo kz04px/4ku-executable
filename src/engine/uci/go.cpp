5564375e74eade3b11f227b88fb728a3571a4237
kz04px <6536046+kz04px@users.noreply.github.com>
2021-07-15 18:30:05 +0100

    Parse wtime + btime, add iterative deepening
    Score of 4ku-new vs 4ku-old: 76 - 0 - 24  [0.880] 100
    Elo difference: 346.12 +/- 71.40
    1+0.01s

--- src/engine/uci/go.cpp
+++ src/engine/uci/go.cpp
@@ -6,11 +6,24 @@
 
 namespace uci {
 
-void go(chess::Position &pos) {
-    const auto bestmove = search::dumb(pos);
-    char bestmove_str[] = "bestmove 123456";
-    chess::move_str(bestmove, &bestmove_str[9], pos.flipped);
-    puts(bestmove_str);
+void go(chess::Position &pos, const int time) {
+    char movestr[6];
+
+    const auto stop_time = clock() + time / 30'000.0f * CLOCKS_PER_SEC;
+
+    for (int i = 1; i < 128; ++i) {
+        chess::Move bestmove;
+        search::negamax(pos, i, bestmove, stop_time);
+
+        // Did we run out of time?
+        if (clock() >= stop_time) {
+            break;
+        }
+
+        chess::move_str(bestmove, movestr, pos.flipped);
+    }
+
+    printf("bestmove %s\n", movestr);
 }
 
 }  // namespace uci
