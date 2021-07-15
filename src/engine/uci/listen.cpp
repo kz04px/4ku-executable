4fda4a1b7b54ad6273242f5d0d488dfdc89e4f15
kz04px <6536046+kz04px@users.noreply.github.com>
2021-07-15 16:30:23 +0100

    Merge UCI moves into listen
    -44 bytes

--- src/engine/uci/listen.cpp
+++ src/engine/uci/listen.cpp
@@ -1,12 +1,17 @@
 #include "listen.hpp"
 #include <stdio.h>
 #include <string.h>
+#include <chess/makemove.hpp>
+#include <chess/move.hpp>
+#include <chess/movegen.hpp>
 #include <chess/position.hpp>
 
 namespace uci {
 
 void listen() {
+    char word[4096];
     auto pos = chess::Position();
+    chess::Move moves[256];
 
     // Pretend to wait for "uci"
     getchar();
@@ -16,38 +21,46 @@ void listen() {
     // Tell the GUI about us
     puts("id name 4ku\nid author kz04px\nuciok");
 
-    char c;
-    while ((c = getchar()) != EOF) {
-        switch (c) {
-            // quit
-            case 'q':
-                return;
-            // isready
-            case 'i':
-                if (getchar() == 's') {
-                    getchar();  // r
-                    getchar();  // e
-                    getchar();  // a
-                    getchar();  // d
-                    getchar();  // y
-                    isready();
+    while (true) {
+        // Get next word
+        char *ptr = word;
+        char c;
+        while ((c = getchar())) {
+            if (c == '\n' || c == '\0' || c == ' ') {
+                break;
+            }
+            *ptr = c;
+            ptr++;
+        }
+        *ptr = '\0';
+
+        // quit
+        if (word[0] == 'q') {
+            break;
+        }
+        // isready
+        else if (word[0] == 'i' && word[1] == 's') {
+            isready();
+        }
+        // go
+        else if (word[0] == 'g' && word[1] == 'o') {
+            go(pos);
+        }
+        // position
+        else if (word[0] == 'p' && word[1] == 'o') {
+            pos = chess::Position();
+        }
+        // Try parse a move
+        else {
+            const int num_moves = chess::movegen(pos, moves);
+            for (int i = 0; i < num_moves; ++i) {
+                char movestr[6];
+                chess::move_str(moves[i], movestr, pos.flipped);
+                if (strncmp(movestr, word, strlen(movestr)) == 0) {
+                    chess::makemove(pos, moves[i]);
+                    break;
                 }
-                break;
-            // go
-            case 'g':
-                if (getchar() == 'o') {
-                    go(pos);
-                }
-                break;
-            // moves
-            case 'm':
-                getchar();  // o
-                getchar();  // v
-                getchar();  // e
-                getchar();  // s
-                pos = chess::Position();
-                moves(pos);
-                break;
+            }
         }
     }
 }
