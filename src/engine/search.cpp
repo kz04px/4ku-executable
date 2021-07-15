53186620df43b34677dcf26d04cd23f9c9ade802
kz04px <6536046+kz04px@users.noreply.github.com>
2021-07-15 15:32:50 +0100

    Material only eval, 1 ply search
    Score of 4ku-new vs 4ku-old: 380 - 0 - 620  [0.690] 1000
    Elo difference: 138.99 +/- 12.21
    +92 bytes

--- src/engine/search.cpp
+++ src/engine/search.cpp
@@ -8,9 +8,28 @@
 
 namespace search {
 
+const int material[] = {100, 300, 325, 500, 900};
+
+[[nodiscard]] int eval(const chess::Position &pos) {
+    int score = 0;
+
+    for (int c = 0; c < 2; ++c) {
+        // Material
+        for (int p = 0; p < 5; ++p) {
+            score += material[p] * chess::count(pos.pieces[p] & pos.colour[c]);
+        }
+
+        score = -score;
+    }
+
+    return score;
+}
+
 [[nodiscard]] chess::Move dumb(const chess::Position &pos) {
     chess::Move moves[256];
     const int num_moves = chess::movegen(pos, moves);
+    int best_score = -9999999;
+    int best_move_idx = 0;
 
     for (int i = 0; i < num_moves; ++i) {
         auto npos = pos;
@@ -20,10 +39,15 @@ namespace search {
             continue;
         }
 
-        return moves[i];
+        const auto score = -eval(npos);
+
+        if (score > best_score) {
+            best_score = score;
+            best_move_idx = i;
+        }
     }
 
-    return {};
+    return moves[best_move_idx];
 }
 
 }  // namespace search
