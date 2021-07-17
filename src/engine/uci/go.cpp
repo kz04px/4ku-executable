f21c72627df99f20278713f7e0faa8683f14c4d5
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2021-07-17 03:15:27 +0300

    Added PV move ordering (#2)
    Score of 4ku vs 4ku-strength-old: 399 - 0 - 1  [0.999] 400
    ...      4ku playing White: 199 - 0 - 1  [0.998] 200
    ...      4ku playing Black: 200 - 0 - 0  [1.000] 200
    ...      White vs Black: 199 - 200 - 1  [0.499] 400
    Elo difference: 1161.0 +/- nan, LOS: 100.0 %, DrawRatio: 0.3 %
    
    Co-authored-by: Gediminas Masaitis <gmasaitis@xirgo.com>

--- src/engine/uci/go.cpp
+++ src/engine/uci/go.cpp
@@ -10,17 +10,16 @@ void go(chess::Position &pos, const int time) {
     char bestmove_str[] = "bestmove 123456";
 
     const auto stop_time = clock() + time / 30'000.0f * CLOCKS_PER_SEC;
-
+    chess::Move pvline[128];
     for (int i = 1; i < 128; ++i) {
-        chess::Move bestmove;
-        search::alphabeta(pos, -INF, INF, i, bestmove, stop_time);
+        search::alphabeta(pos, -INF, INF, i, 0, stop_time, pvline);
 
         // Did we run out of time?
         if (clock() >= stop_time) {
             break;
         }
 
-        chess::move_str(bestmove, &bestmove_str[9], pos.flipped);
+        chess::move_str(pvline[0], &bestmove_str[9], pos.flipped);
     }
 
     puts(bestmove_str);
