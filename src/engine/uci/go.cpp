e5e2639833e1dc1acfda4e011e4f3e0105ca62e6
kz04px <6536046+kz04px@users.noreply.github.com>
2021-07-24 21:04:06 +0100

    Remove unused variable

--- src/engine/uci/go.cpp
+++ src/engine/uci/go.cpp
@@ -10,7 +10,6 @@ void go(chess::Position &pos, const int time) {
     const auto stop_time = now() + time / 30;
     search::StackData ss[128];
     char bestmove_str[] = "bestmove 123456";
-    chess::Move bestmove;
 
     for (int i = 0; i < 128; ++i) {
         search::search(pos, -search::inf, search::inf, i, ss, stop_time);
